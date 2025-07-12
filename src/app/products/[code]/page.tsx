a74a7801c9916cd3d955b16fcac6872a6e0868ea
sijun <sionwer5@gmail.com>
2025-07-12 21:22:01 +0900

    refactor: 사용하지 않는 코드 제거

--- src/app/products/[code]/page.tsx
+++ src/app/products/[code]/page.tsx
@@ -1,15 +1,6 @@
 import Link from 'next/link';
 import {getProduct} from "@/api/product";
 
-function formatViews(views: number): string {
-    if (views >= 1000000) {
-        return (views / 1000000).toFixed(1) + 'M';
-    } else if (views >= 1000) {
-        return (views / 1000).toFixed(1) + 'K';
-    }
-    return views.toString();
-}
-
 export default async function ProductDetailPage({
                                                     params
                                                 }: {
